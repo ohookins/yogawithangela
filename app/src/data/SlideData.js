56fa5bc37298d8035698f1a81f84dadedb51f84b
Oliver Hookins <ohookins@gmail.com>
2014-08-16 23:01:08 +0200

    Add yeoman-style scaffold with bower and grunt.

--- app/src/data/SlideData.js
+++ app/src/data/SlideData.js
@@ -2,23 +2,23 @@ define(function(require, exports, module) {
     var SlideData = {};
 
     SlideData.imageURLs = [
-        { url: "img/9065-web.jpg",
+        { url: "/content/images/9065-web.jpg",
           startOffset: -150,
           endOffset: -300
         },
-        { url: "img/9078-web.jpg",
+        { url: "/content/images/9078-web.jpg",
           startOffset: -100,
           endOffset: -270
         },
-        { url: "img/9081-web.jpg",
+        { url: "/content/images/9081-web.jpg",
           startOffset: -150,
           endOffset: -320
         },
-        { url: "img/9083-web.jpg",
+        { url: "/content/images/9083-web.jpg",
           startOffset: -50,
           endOffset: -380
         },
-        { url: "img/9089-web.jpg",
+        { url: "/content/images/9089-web.jpg",
           startOffset: -80,
           endOffset: -330,
         }
